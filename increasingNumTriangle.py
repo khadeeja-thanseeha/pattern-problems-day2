2a5ebaf30132741dd96b61f4f6560dd66bd777cf
Khadeeja Thanseeha V <117475589+khadeeja-thanseeha@users.noreply.github.com>
2025-06-13 18:23:04 +0530

    increasingNumTriangle.py

---
+++ increasingNumTriangle.py
@@ -0,0 +1,7 @@
+def nNumberTriangle(n: int) -> None:
+    num=1
+    for i in range(1,n+1,1):
+        for j in range(1,i+1,1):
+            print(num,end=(" "))
+            num+=1
+        print("")
